253f28af35a338051502ea600767378c86155200
Kang0413 <whw2708@gmail.com>
2026-01-15 14:09:51 +0900

    Update PyArduino.py

--- PyArduino.py
+++ PyArduino.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import numpy as np
 from telemetrix_uno_r4.wifi.telemetrix_uno_r4_wifi import telemetrix_uno_r4_wifi
 from telemetrix_uno_r4.minima.telemetrix_uno_r4_minima import telemetrix_uno_r4_minima
 
@@ -54,7 +55,7 @@ class PyArduino():
         """
         global digital_value
         digital_value = None
-        self.board.set_pin_mode_digital_input_pullup(pin_number, callback = self.get_digital_state_slicer)
+        self.board.set_pin_mode_digital_input_pullup(pin_number, callback = self._get_digital_state_slicer)
         
         while digital_value is None:
             time.sleep(0.01)
@@ -63,7 +64,39 @@ class PyArduino():
 
         return digital_value
 
-    def get_digital_state_slicer(self, data):
+    def run_digital_pwm_write(self, pin_number : int, pwm_intensity_percentage : int):
+        max_value = 256
+        self.board.set_pin_mode_analog_output(pin_number)
+        self.board.analog_write(pin_number, round(pwm_intensity_percentage / 100 * max_value))
+        time.sleep(.005)
+        # self.board.set_pin_mode_digital_output(pin_number)
+
+    def get_analog_state(self, pin_number : int):
+        """
+        This function read arduino analog pins. Double checking with any pin is recommended.
+
+        Input : int
+        Output : int
+        """
+        global analog_value
+
+        analog_value = None
+        self.board.set_pin_mode_analog_input(pin_number, differential = 0, callback = self._get_analog_state_slicer)
+        while analog_value is None:
+            time.sleep(0.01)
+
+        return analog_value
+    
+    def run_pump_speed(self, target_speed: int):
+        """
+        This function simplify usaage of MCP4728 controller. Max speed is limited to 20.
+
+        Input : int
+        """
+        speed = np.clip(target_speed, 0, 20)
+        self._run_MCP4728_control(1, speed)
+        
+    def _get_digital_state_slicer(self, data):
         """
         This internal function slicing arduino's output datastream.
 
@@ -77,23 +110,7 @@ class PyArduino():
 
         digital_value = data[pin_state_index]
 
-    def get_analog_state(self, pin_number : int):
-        """
-        This function read arduino analog pins. Double checking with any pin is recommended.
-
-        Input : int
-        Output : int
-        """
-        global analog_value
-
-        analog_value = None
-        self.board.set_pin_mode_analog_input(pin_number, differential = 0, callback = self.get_analog_state_slicer)
-        while analog_value is None:
-            time.sleep(0.01)
-
-        return analog_value
-
-    def get_analog_state_slicer(self, data):
+    def _get_analog_state_slicer(self, data):
         """
         This internal function slicing arduino's output datastream.
 
@@ -108,7 +125,19 @@ class PyArduino():
         # print(data[pin_state_index])
 
         analog_value = data[pin_state_index]
-    
+
+    def _run_MCP4728_control(self, pin_number : int, voltage_percentage : int):
+        '''
+        This function communicate MCP4728 with IIC communication. you may need to update your repo to newest version.
+
+        Input : int, int
+        '''
+        max_value = 255
+        voltage_factor = 5 / 3.3
+        error_compensate_percentage = voltage_percentage / 50
+        self.board.MCP4728_control(pin_number, round((voltage_percentage + error_compensate_percentage) / 100 * max_value * voltage_factor))
+        time.sleep(.005)
+
 def main():
     """
     This function holds example main flow
@@ -116,13 +145,30 @@ def main():
     Input : None
     Output : None
     """
-    pa = PyArduino()
-    # 센서 예제
+    pa = PyArduino("minima")
     while True:
-        value0 = pa.get_analog_state(0)
-        print("A0 is " + str(value0))
-        value1 = pa.get_analog_state(1)
-        print("A1 is " + str(value1))
+        pa.run_pump_speed(10)
+        time.sleep(3)
+        pa.run_pump_speed(10)
+        time.sleep(3)
+        # pa._run_MCP4728_control(1, 20)
+        # time.sleep(3)
+        # pa._run_MCP4728_control(1, 60)
+        # time.sleep(3)
+    # pin_number_power = 10
+    # pin_number_rpm = 11
+    # pa.run_digital_write(pin_number_power, False)
+    # while True:
+    #     pa.run_digital_pwm_write(pin_number_rpm, 100)
+    #     time.sleep(3)
+    #     pa.run_digital_pwm_write(pin_number_rpm, 0)
+    #     time.sleep(3)
+    # 센서 예제
+    # while True:
+    #     value0 = pa.get_analog_state(0)
+    #     print("A0 is " + str(value0))
+    #     value1 = pa.get_analog_state(1)
+    #     print("A1 is " + str(value1))
     # while True:
     #     value = pa.get_digital_state(8)
     #     print(value)
